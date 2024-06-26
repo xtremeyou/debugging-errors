c350e162cee719440ace37993a3ab66d4d80a60d
Merak Bourque <xtremeyou@hotmail.com>
2024-06-25 21:38:59 -0400

    Added a reversing function program

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
 var input = process.argv[2];
 
-if (input)
-  console.log(reverse(input));
-
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  if (input) {
+    return original.split('').reverse().join('');
+  }
 }
+console.log(reverse(input));
